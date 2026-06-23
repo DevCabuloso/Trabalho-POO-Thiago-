57f4d253af72d1e9b4ce93256f130d490213336f
Karlos Eduardo Nunes de Souza <karlosnunesdasilva4@gmail.com>
2026-06-23 10:01:01 -0300

    Arrumei isso aq

--- Main.java
+++ Main.java
@@ -1,34 +1,30 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+
         Integer[] numeros = {1,2,2,4,2};
 
-        System.out.println("Quantidade: "+
-                          ArrayUtil.count(numeros,2)
+        System.out.println(
+            "Quantidade de vezes que o dois aparece: " +
+            ArrayUtil.count(numeros, 2)
         );
 
         String[] nomes = {
-                "Ana",
-                "Pedro",
-                "Ana"
+            "Ana",
+            "Pedro",
+            "Ana"
         };
-
-         System.out.println(
-                  "Quantidade: "+
-                  ArrayUtil.count(nomes,"Ana")
-        );
 
         String[] materiais = {
-                "cadernos",
-                "lapis",
-                "Cadernos",
-                "Cadernos",
-                "Borracha"
+            "cadernos",
+            "lapis",
+            "cadernos",
+            "adernos",
+            "Borracha"
         };
 
-         System.out.println(
-                  "Quantidade: "+
-                  ArrayUtil.count(materiais,"cadernos")
+        System.out.println(
+            "Quantidade de Ana: " + ArrayUtil.count(nomes, "Ana") +
+            " | Quantidade de lapis: " + ArrayUtil.count(materiais, "lapis")
         );
-
     }
 }
